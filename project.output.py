04a4f995d4563b2a5b655a3cde5b5fb031999c0d
ashk2324 <95518215+ashk2324@users.noreply.github.com>
2021-12-13 23:12:10 +0530

    Add files via upload

---
+++ project.output.py
@@ -0,0 +1,15 @@
+Python 3.7.6 (tags/v3.7.6:43364a7ae0, Dec 18 2019, 23:46:00) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Admin/AppData/Local/Programs/Python/Python37-32/bio-project.py
+Enter a file name >	Variola.txt
+
+
+Motif generation successful.
+
+Motif length: 3
+
+Motif: CAT
+Enter a file name to be searched >	Variola.txt
+Given motif is found at index:	 48
+>>> 
